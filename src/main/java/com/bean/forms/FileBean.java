8044a36d68a91ecea2dfbc6bb7183d580df1144f
jigar_2305 <jigarkp786@gmail.com>
2022-09-18 14:29:38 +0530

    excel

---
+++ src/main/java/com/bean/forms/FileBean.java
@@ -0,0 +1,13 @@
+package com.bean.forms;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Data;
+
+
+@Data
+public class FileBean {
+	
+	private MultipartFile file;
+
+}
---
+++ src/main/java/com/controller/admin/FilereadController.java
@@ -0,0 +1,38 @@
+package com.controller.admin;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.bean.ResponseBean;
+import com.bean.forms.QuestionBean;
+import com.service.QuestionService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("file")
+public class FilereadController {
+
+	@Autowired
+	QuestionService questionService;
+
+	@PostMapping("excel")
+	public ResponseEntity<?> fileread(@RequestParam("file") MultipartFile excel) {
+
+		List<QuestionBean> questions = questionService.addquestion(excel);
+
+		ResponseBean<List<QuestionBean>> res = new ResponseBean<>();
+		res.setData(questions);
+		res.setMsg("question added successfully..");
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(res);
+	}
+
+}
